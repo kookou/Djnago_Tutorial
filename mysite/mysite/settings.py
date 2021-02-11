95fb293dccd7d34addd0afb368b08aaf78cba637
kookuo <nnaangee@gmail.com>
2021-02-12 04:15:41 +0900

    Secret Key 삭제

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l9==(y^of#mk1^u_gnbq5h$rit(uj(3pq84q=bns_*03m4sf98'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
